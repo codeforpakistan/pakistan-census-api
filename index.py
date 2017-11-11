50f8abfec61dbf94e67ba6037058cf0539271360
Mubassir Hayat <mubassirhayat@gmail.com>
2017-11-11 15:46:22 +0500

    Intial Commit

---
+++ index.py
@@ -0,0 +1,42 @@
+from flask import Flask
+from flask import request
+from flask.ext.jsonpify import jsonify
+from flask_restful import Api
+from flask_restful import Resource
+from json import dumps
+from sqlalchemy import create_engine
+
+# db_connect = create_engine('sqlite:///chinook.db')
+connection = MongoClient("mongodb://mubbi:123456@ds115625.mlab.com:15625/pakistancensus")
+
+app = Flask(__name__)
+api = Api(app)
+
+class Employees(Resource):
+    def get(self):
+        conn = db_connect.connect() # connect to database
+        query = conn.execute("select * from employees") # This line performs query and returns json result
+        return {'employees': [i[0] for i in query.cursor.fetchall()]} # Fetches first column that is Employee ID
+
+class Tracks(Resource):
+    def get(self):
+        conn = db_connect.connect()
+        query = conn.execute("select trackid, name, composer, unitprice from tracks;")
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+class Employees_Name(Resource):
+    def get(self, employee_id):
+        conn = db_connect.connect()
+        query = conn.execute("select * from employees where EmployeeId =%d "  %int(employee_id))
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return jsonify(result)
+
+
+api.add_resource(Employees, '/employees') # Route_1
+api.add_resource(Tracks, '/tracks') # Route_2
+api.add_resource(Employees_Name, '/employees/<employee_id>') # Route_3
+
+
+if __name__ == '__main__':
+     app.run(port='5002')
