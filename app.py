ce045f3fb9d3ef18c7ec4300f253cf52aa9c3fea
Ali Raza <ali@sweetpixelstudios.com>
2017-12-11 20:40:52 +0500

    added dynamic port and host mapping

--- app.py
+++ app.py
@@ -5,18 +5,17 @@ from flask_restful import Resource
 from json import dumps
 from pymongo import MongoClient
 import pandas as pd
+import os
 
 app = Flask(__name__)
 api = Api(app)
 
 class Provinces(Resource):
-    def get(self):
+  def get(self):
+    provinces_df = pd.read_excel("DISTRICT_WISE_CENSUS_RESULTS_CENSUS_2017.xlsx","Province_Wise")
+    print provinces_df
 
-
-        provinces_df = pd.read_excel("DISTRICT_WISE_CENSUS_RESULTS_CENSUS_2017.xlsx","Province_Wise")
-        print provinces_df
-
-        return provinces_df.to_dict(orient='records')
+    return provinces_df.to_dict(orient='records')
 
 
 
@@ -25,4 +24,5 @@ api.add_resource(Provinces, '/provinces') # Route_1
 
 
 if __name__ == '__main__':
-     app.run(port=5002,debug=True)
+  port = int(os.environ.get('PORT', 5002))
+  app.run(host='0.0.0.0',port=5002,debug=True)
